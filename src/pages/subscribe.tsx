2d6b3886922bfc4d9a0bd9a9c1c7a01b7b595d09
heypran <pranjal.baweja@gmail.com>
2023-04-27 01:23:45 +0530

    fix: substack link

--- src/pages/subscribe.tsx
+++ src/pages/subscribe.tsx
@@ -1,53 +1,21 @@
 import React from 'react';
-import { Box, Button, Container, Heading, Input, Text } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import { Main } from '@templates/Main';
 import { Meta } from '@layout/Meta';
 
-export const SIGNUP_FORM_ACTION =
-  'https://www.getrevue.co/profile/zkblock/add_subscriber';
-export const SIGNUP_EMAIL_INPUT_NAME = 'member[email]';
+export const SIGNUP_FORM_ACTION = 'https://zkblock.substack.com/';
+export const SIGNUP_EMAIL_INPUT_NAME = 'email';
 
 export function FreeSignUp() {
   return (
-    <Box p="20px" rounded="5px" borderWidth={2}>
-      <Box textAlign="left">
-        <Heading mb="10px" fontSize="23px" fontWeight={700}>
-          Zk Updates
-        </Heading>
-        <Text mb="14px" fontSize="14px" lineHeight="20px">
-          Enter your email below to get notified about the new zero knowledge
-          projects, guides and updates
-        </Text>
-
-        <form action={SIGNUP_FORM_ACTION} method="post" target="_blank">
-          <Input
-            size="sm"
-            fontSize="15px"
-            py="18px"
-            rounded="4px"
-            placeholder="Your email"
-            borderWidth={2}
-            mb={'10px'}
-            name={SIGNUP_EMAIL_INPUT_NAME}
-          />
-          <Button
-            type={'submit'}
-            bg="gray.700"
-            _hover={{ bg: 'black' }}
-            fontWeight={500}
-            color={'white'}
-            w="100%"
-          >
-            Subscribe
-          </Button>
-        </form>
-
-        <Text color="gray.500" fontSize="12px" mt="10px">
-          <Text as="span">We will only send emails</Text> when there are new
-          updates.
-        </Text>
-      </Box>
-    </Box>
+    <iframe
+      src="https://zkblock.substack.com/embed"
+      width="480"
+      height="320"
+      style={{ border: '1px solid #EEE', background: 'white' }}
+      frameBorder="0"
+      scrolling="no"
+    ></iframe>
   );
 }
 
