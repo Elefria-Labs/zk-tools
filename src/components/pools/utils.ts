dea2dc079d7ab84cfb0d455029bdbf2457f0401b
heypran <pranjal.baweja@gmail.com>
2023-09-30 08:08:08 +0200

    feat: math helpers

--- src/components/pools/utils.ts
+++ src/components/pools/utils.ts
@@ -63,3 +63,36 @@ export interface Pool {
   token0Price: string;
   token1Price: string;
 }
+
+const expandDecimals = (n: number | string | bn, exp: number): bn => {
+  return new bn(n).multipliedBy(new bn(10).pow(exp));
+};
+const encodeSqrtPriceX96 = (price: number | string | bn): bn => {
+  return new bn(price).sqrt().multipliedBy(Q96).integerValue(3);
+};
+const mulDiv = (a: bn, b: bn, multiplier: bn) => {
+  return a.multipliedBy(b).div(multiplier);
+};
+
+export const getPriceFromTick = (
+  tick: number,
+  token0Decimal: string,
+  token1Decimal: string,
+): number => {
+  const sqrtPrice = new bn(Math.pow(Math.sqrt(1.0001), tick)).multipliedBy(
+    new bn(2).pow(96),
+  );
+  const token0 = expandDecimals(1, Number(token0Decimal));
+  const token1 = expandDecimals(1, Number(token1Decimal));
+  const L2 = mulDiv(
+    encodeSqrtPriceX96(token0),
+    encodeSqrtPriceX96(token1),
+    Q96,
+  );
+  const price = mulDiv(L2, Q96, sqrtPrice)
+    .div(new bn(2).pow(96))
+    .div(new bn(10).pow(token0Decimal))
+    .pow(2);
+
+  return price.toNumber();
+};
