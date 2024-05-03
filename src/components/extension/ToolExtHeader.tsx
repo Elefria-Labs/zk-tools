62c0ea8bf2ff83d1e8b22db67c222cea5deedf77
heypran <pranjal.baweja@gmail.com>
2024-05-03 18:24:56 +0200

    feat: extension changes

--- src/components/extension/ToolExtHeader.tsx
+++ src/components/extension/ToolExtHeader.tsx
@@ -3,40 +3,37 @@ import {
   Card,
   CardBody,
   Flex,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
   IconButton,
+  useDisclosure,
+  Button,
 } from '@chakra-ui/react';
-import { EditIcon, ExternalLinkIcon, HamburgerIcon } from '@chakra-ui/icons';
+import { HamburgerIcon } from '@chakra-ui/icons';
 import GasPrice from './GasPrice';
+import ToolExtMenuDrawer from './ToolExtMenuDrawer';
 
 export default function ToolExtHeader() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef(null);
   return (
     <Flex alignItems="center">
       <Card w="100%" roundedTop={0}>
         <CardBody p={4} py={2}>
           <Flex justifyContent="flex-end" alignItems="center">
             <GasPrice />
-            <Menu>
-              <MenuButton
-                ml={8}
-                as={IconButton}
-                aria-label="Options"
-                icon={<HamburgerIcon />}
-                variant="outline"
-              />
-              <MenuList>
-                <MenuItem icon={<ExternalLinkIcon />}>Web App</MenuItem>
-                <MenuItem icon={<EditIcon />}>Feedback</MenuItem>
-                {/*             
-              <MenuItem icon={<Contribte />} >
-                Contribute
-              </MenuItem>
-               */}
-              </MenuList>
-            </Menu>
+            <Button
+              ml={8}
+              as={IconButton}
+              aria-label="Options"
+              icon={<HamburgerIcon />}
+              variant="outline"
+              ref={btnRef}
+              onClick={onOpen}
+            />
+            <ToolExtMenuDrawer
+              onClose={onClose}
+              isOpen={isOpen}
+              btnRef={btnRef}
+            />
           </Flex>
         </CardBody>
       </Card>
